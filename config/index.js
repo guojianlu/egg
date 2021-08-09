5c17918e7035a3f3143c216d110efe01d5447340
guojianlu <jianluguo@yunify.com>
2021-08-10 00:16:08 +0800

    style: format code

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
   db: {
-    dialect:'mysql',
-    host:'localhost',
-    database:'kaikeba',
-    username:'root',
-    password:'example'
+    dialect: 'mysql',
+    host: 'localhost',
+    database: 'test',
+    username: 'root',
+    password: '123456'
   },
   middleware: ['errorHandler', 'logger']  // 以数组形式，保证执行顺序
 }
